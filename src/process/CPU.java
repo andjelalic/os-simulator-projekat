a2e2f884763e048b620c7dca8fc87876719b3e3a
AndjeLalic <lallicandjela@gmail.com>
2026-09-03 23:37:22 +0200

    cpu logika

--- src/process/CPU.java
+++ src/process/CPU.java
@@ -4,21 +4,45 @@ public class CPU {
 
     private PCB current;
     private long cycleCount;
+    private int stepsInBurst;
+    private int quantum;
 
-    public CPU() {
-        // TODO
+    public CPU(int quantum) {
+        this.quantum = quantum;
+        this.cycleCount = 0;
+        this.stepsInBurst = 0;
+        this.current = null;
     }
 
     public void executeOneStep() {
-        // TODO
+        if (isIdle()) {
+            return;
+        }
+
+        current.setState(ProcessState.RUNNING);
+        current.setRemainingTime(current.getRemainingTime() - 1);
+        cycleCount++;
+        stepsInBurst++;
+
+        if (current.getRemainingTime() <= 0) {
+            current.setState(ProcessState.TERMINATED);
+        }
     }
 
     public void contextSwitch(PCB next) {
-        // TODO
+        current = next;
+        stepsInBurst = 0;
     }
 
     public PCB getCurrent() {
-        // TODO
-        return null;
+        return current;
+    }
+
+    public boolean isQuantumExpired() {
+        return stepsInBurst >= quantum;
+    }
+
+    public boolean isIdle() {
+        return current == null || current.getState() == ProcessState.TERMINATED;
     }
 }
